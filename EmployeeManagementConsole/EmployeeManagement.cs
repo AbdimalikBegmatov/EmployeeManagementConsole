deab1ddebad56fde9eaa51ad8a90b961ec55ab51
AbdimalikBegmatov <abdimalik.b@gmail.com>
2023-07-26 08:58:53 +0600

    Добавьте файлы проекта.

---
+++ EmployeeManagementConsole/EmployeeManagement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementConsole
+{
+    public class EmployeeManagement
+    {
+        List<Employee> employees=new();
+        int count = 0;
+
+
+        public string AddEmployee()
+        {
+            Employee employee = new();
+            employee.Id = count++;
+            Console.WriteLine("Write Employee name:");
+            employee.Name = Console.ReadLine().Trim();
+            Console.WriteLine("Write Employee age:");
+            employee.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write Employee position:");
+            employee.Position = Console.ReadLine().Trim();
+
+            employees.Add(employee);
+            return "Employees added";
+        }
+
+        public string RemoveEmployee(int id)
+        {
+            var result = employees.FirstOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                return "Employee not found";
+            }
+            employees.Remove(result);
+            return "Employee deleted";
+        }
+
+        public void GetAllEmployees() {
+            if (employees.Count == 0)
+            {
+                Console.WriteLine("Employees empty");
+            }
+            foreach (var item in employees)
+            {
+                Console.WriteLine($"Id: {item.Id} - name: {item.Name} - age: {item.Age} - position: {item.Position}");
+            }
+        }
+    }
+}
---
+++ EmployeeManagementConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeManagementConsole;
+
+EmployeeManagement employeeManagement = new();
+bool stopProgram = true;
+
+while (stopProgram)
+{
+    Console.WriteLine("----------------------------------------------------------------------");
+    Console.WriteLine("Choose action with Employees");
+	Console.WriteLine("1. Add new Employee, 2. Delete Employee, 3. Get all Employees, 4. Exit");
+
+
+    int action = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("----------------------------------------------------------------------");
+
+    switch (action)
+	{
+		case 1:
+			employeeManagement.AddEmployee();
+			break;
+		case 2:
+			Console.WriteLine("Write id employee to delete");
+			var result = employeeManagement.RemoveEmployee(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(result);
+            break;
+		case 3:
+			employeeManagement.GetAllEmployees();
+			break;
+		default:
+			stopProgram=false;
+			break;
+	}
+    Console.WriteLine("-----------------------------------------------------------------------");
+}
